d2dc0518b319ca1be0fd2b1d96c54f422ae2c851
yumKing <jy11215288@163.com>
2018-05-12 13:38:38 +0800

    add 航空公司客户类型识别 预处理过程

--- lesson_2/data_retreatment.py
+++ lesson_2/data_retreatment.py
@@ -10,11 +10,11 @@ result_path = 'data/retreatment.xls'
 
 data = pd.read_excel(file_path)
 
-#percentiles:指定计算多少的分位数表(如 1/4分位数，中位数)
-retreatment = data.describe(percentiles=[],include='all').T
-#count表示的是非空的数量
+# percentiles:指定计算多少的分位数表(如 1/4分位数，中位数)
+retreatment = data.describe(percentiles=[], include='all').T
+# count表示的是非空的数量
 retreatment['null'] = len(data) - retreatment['count']
 
-retreatment = retreatment[['null','min','max']]
-retreatment.columns=['空值','最小值','最大值']
+retreatment = retreatment[['null', 'min', 'max']]
+retreatment.columns = ['空值', '最小值', '最大值']
 retreatment.to_excel(result_path)
